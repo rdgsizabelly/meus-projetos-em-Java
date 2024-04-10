3ec3efe097fef107f9647f76f2288627473f3f53
Izabelly Rodrigues <149176367+rdgsizabelly@users.noreply.github.com>
2024-04-09 21:11:42 -0300

    Praticando Operador ternario

---
+++ OperadorTernario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorternario;
+
+/**
+ *
+ * @author BELA
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 2, n2 =1, maior;
+        maior = n1<n2 ? 0 : 1;
+        System.out.println(maior);
+    }
+    
+}
