05533e123d601e70f6f9e0c975bff67776e0b197
Izabelly Rodrigues <149176367+rdgsizabelly@users.noreply.github.com>
2024-04-10 11:46:12 -0300

    Criando um controle remoto com interface e herança

---
+++ JAVA POO - CR Controle.java
@@ -0,0 +1,17 @@
+
+package encapsulamento;
+
+public class Encapsulamento {
+
+    
+    public static void main(String[] args) {
+        
+        ControleRemoto c = new ControleRemoto();
+        
+        c.ligar();
+        c.abrirMenu();
+        c.desligarMudo();
+        c.fecharMenu();
+    }
+    
+}
